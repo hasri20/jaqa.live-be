0d496f688e6fdafa0ca9deb8e0ab1169d8ce224c
hasri20 <ryugahasry@gmail.com>
2021-01-20 11:27:45 +0700

    routes api for users

---
+++ migrations/20210119073901-create-user.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.STRING
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      question_count: {
+        type: Sequelize.NUMBER
+      },
+      following_count: {
+        type: Sequelize.NUMBER
+      },
+      followers_count: {
+        type: Sequelize.NUMBER
+      },
+      bio: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
